a0ce0229d72255faa4bd789a17151dfa6a2f4087
Pier-Luc Michaud <pier-luc.michaud@mediaclip.ca>
2018-06-24 23:26:21 -0400

    Rename steps for readability

--- FluentBDD.Samples/ATMSample/ATMSteps.cs
+++ FluentBDD.Samples/ATMSample/ATMSteps.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using FluentAssertions;
+﻿using FluentAssertions;
 
 namespace FluentBDD.Samples.ATMSample
 {
@@ -12,26 +11,26 @@ namespace FluentBDD.Samples.ATMSample
             _scenarioContext = scenarioContext;
         }
         
-        public void GivenAUserHasXDollars(int amount)
+        public void Given_a_user_has_x_dollars(int amount)
         {
             var atm = new ATMSession();
             _scenarioContext.Add(atm);
             atm.Deposite(100);
         }
 
-        public void WhenTheUserWithdrawsX(int X)
+        public void When_the_user_withdraws_x(int x)
         {
-            _scenarioContext.Get<ATMSession>().Withdraw(X);
+            _scenarioContext.Get<ATMSession>().Withdraw(x);
         }
 
-        public void WhenTheUserDepositsX(int X)
+        public void When_the_user_deposits_x(int x)
         {
-            _scenarioContext.Get<ATMSession>().Deposite(X);
+            _scenarioContext.Get<ATMSession>().Deposite(x);
         }
 
-        public void ThenTheUserHasX(int X)
+        public void Then_the_user_has_x(int x)
         {
-            _scenarioContext.Get<ATMSession>().GetBalance().Should().Be(X);
+            _scenarioContext.Get<ATMSession>().GetBalance().Should().Be(x);
         }
     }
 }
--- FluentBDD.Samples/ATMSample/ATMTests.cs
+++ FluentBDD.Samples/ATMSample/ATMTests.cs
@@ -11,14 +11,14 @@ namespace FluentBDD.Samples.ATMSample
         }
 
         [Fact]
-        public void CanDoStandardATMTransactions()
+        public void Can_do_standard_ATM_transactions()
         {
             Scenario.Create()
-                .Step<ATMSteps>(s => s.GivenAUserHasXDollars(100))
-                .Step<ATMSteps>(s => s.WhenTheUserWithdrawsX(75))
-                .Step<ATMSteps>(s => s.ThenTheUserHasX(25))
-                .Step<ATMSteps>(s => s.WhenTheUserDepositsX(5))
-                .Step<ATMSteps>(s => s.ThenTheUserHasX(30));
+                .Step<ATMSteps>(s => s.Given_a_user_has_x_dollars(100))
+                .Step<ATMSteps>(s => s.When_the_user_withdraws_x(75))
+                .Step<ATMSteps>(s => s.Then_the_user_has_x(25))
+                .Step<ATMSteps>(s => s.When_the_user_deposits_x(5))
+                .Step<ATMSteps>(s => s.Then_the_user_has_x(30));
         }
     }
 }
